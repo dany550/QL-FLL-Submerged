2cb1cda7369562eefee0a9e7e9c8247c68247bb5
dany550 <dany.valek@seznam.cz>
2024-11-01 00:07:50 +0100

    2.0.3 after the rules
    - adds todo list

---
+++ documentation.py
@@ -0,0 +1,7 @@
+to do:
+    1. to clear the code and standartize sintax
+    2. to add arm operating function and other
+    3. to develop position anti-fail system (accelerometer based)
+    4. to develop mission anti-fail system (optimalization in fail situations)
+    5. if time to experiment with multitasking
+    6. to create the best code ever!
---
+++ ride2.py
@@ -0,0 +1,11 @@
+from tools_II import *
+
+hub = PrimeHub()
+Lw = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+Rw = Motor(Port.B)
+La = Motor(Port.C)
+Ra = Motor(Port.D)
+bot = Robot(hub, 27.9, 158, Lw, Rw)
+bot.set_origin(2000 - 148, 159, 90, [[0,0],[2000, 1140]])
+
+bot.straight_position()
