0304e9c72d6ec32f9331f7cbbd60f709130d2ba9
H2SO4-TM <richard.stavarsky@gmail.com>
2025-03-07 16:01:39 +0100

    Add files via upload

--- M02_R02.py
+++ M02_R02.py
@@ -7,7 +7,7 @@ m21 = Mission(bot, -615, 370, 180, [0, 300], setup_speed=500)
 def m21_body(): 
     bot.straight_g(30, 500)
     Ra.target(600, 500, False)
-    bot.straight_g(335)
+    bot.straight_g(340)
     Ra.target(200, 500)
     #bot.straight_position(-800, 335, -1)
     #bot.straight_g(-150, 50, True, 150)
@@ -22,10 +22,11 @@ def m211_body():
     bot.straight_g(500, 50, True, 200)
 m211.add_body(m211_body)
 
-m22 = Mission(bot, 280, 630, 180, [0, 500], 1)
+m22 = Mission(bot, 280, 640, 180, [0, 500], 0)
 def m22_body():
     Ra.target(850, 500)
-    bot.straight_g(-230, 100, True, 180, speed=400)
+    
+    bot.straight_g(-280, 100, True, 180, speed=400)
     bot.straight_g(100)
     bot.straight_g(170, 900, True, 225)
     #Ra.target(500, 500, False)
@@ -33,7 +34,7 @@ def m22_body():
 m22.add_body(m22_body)
 m22.add_checkpoint(100,150,-1)
 
-m23 = Mission(bot, 350, 105, 0, [0, 500])
+m23 = Mission(bot, 350, 120, 0, [0, 500])
 def m23_body():
     Ra.target(100, 500)
     bot.straight_g(100)
